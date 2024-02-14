912a6cad282bf16eabe23c6a10dce901767e0a7f
olehhutsuliak <olegucul@gmail.com>
2024-02-14 20:41:10 +0100

    change configs

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -4,7 +4,6 @@ module.exports = {
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:@typescript-eslint/stylistic',
-    'plugin:playwright/recommended',
     'prettier',
   ],
   parser: '@typescript-eslint/parser',
